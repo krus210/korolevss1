d8c472e172e225ccc17bb1c1caf00ffa6c91fed2
stanislav <staskorol21@gmail.com>
2020-04-23 16:07:48 +0300

    change jwt

--- src/main/kotlin/ru/korolevss/service/JWTTokenService.kt
+++ src/main/kotlin/ru/korolevss/service/JWTTokenService.kt
@@ -3,6 +3,7 @@ package ru.korolevss.service
 import com.auth0.jwt.JWT
 import com.auth0.jwt.JWTVerifier
 import com.auth0.jwt.algorithms.Algorithm
+import ru.korolevss.model.UserModel
 
 class JWTTokenService {
     private val secret = "5c2dbef6-289c-46e6-8cfd-d8b3292d373a"
@@ -10,8 +11,9 @@ class JWTTokenService {
 
     val verifier: JWTVerifier = JWT.require(algo).build()
 
-    fun generate(id: Long): String = JWT.create()
-        .withClaim("id", id)
+    fun generate(model: UserModel): String = JWT.create()
+        .withClaim("id", model.id)
+        .withClaim("password", model.password)
         // TODO: Comment this to disable expiration
         //.withExpiresAt(Date(System.currentTimeMillis() + 1000))
         .sign(algo)
--- src/main/kotlin/ru/korolevss/service/UserService.kt
+++ src/main/kotlin/ru/korolevss/service/UserService.kt
@@ -1,9 +1,7 @@
 package ru.korolevss.service
 
 import io.ktor.features.NotFoundException
-import io.ktor.http.HttpStatusCode
 import io.ktor.util.KtorExperimentalAPI
-import kotlinx.coroutines.sync.Mutex
 import org.springframework.security.crypto.password.PasswordEncoder
 import ru.korolevss.dto.AuthenticationRequestDto
 import ru.korolevss.dto.AuthenticationResponseDto
@@ -13,17 +11,14 @@ import ru.korolevss.exception.InvalidPasswordException
 import ru.korolevss.exception.NullUsernameOrPasswordException
 import ru.korolevss.exception.PasswordChangeException
 import ru.korolevss.exception.UserExistsException
-import ru.korolevss.model.MediaModel
 import ru.korolevss.model.UserModel
 import ru.korolevss.repository.UserRepository
-import java.io.IOException
 
 class UserService(
     private val repo: UserRepository,
     private val tokenService: JWTTokenService,
     private val passwordEncoder: PasswordEncoder
 ) {
-    private val mutex = Mutex()
 
     suspend fun getModelById(id: Long): UserModel? {
         return repo.getById(id)
@@ -47,7 +42,7 @@ class UserService(
         }
         val copy = model.copy(password = passwordEncoder.encode(input.new))
         repo.save(copy)
-        val token = tokenService.generate(model.id)
+        val token = tokenService.generate(model)
         return AuthenticationResponseDto(token)
     }
 
@@ -58,7 +53,7 @@ class UserService(
             throw InvalidPasswordException("Wrong password!")
         }
 
-        val token = tokenService.generate(model.id)
+        val token = tokenService.generate(model)
         return AuthenticationResponseDto(token)
     }
 
@@ -69,7 +64,7 @@ class UserService(
             throw UserExistsException("User already exists")
         } else {
             val model = repo.save(UserModel(username = username, password = passwordEncoder.encode(password)))
-            val token = tokenService.generate(model.id)
+            val token = tokenService.generate(model)
             return AuthenticationResponseDto(token)
         }
     }
